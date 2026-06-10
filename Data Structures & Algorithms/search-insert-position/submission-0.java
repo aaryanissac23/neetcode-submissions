b21edf60d9ad982b1e65f8112df7cfe342ede27c
aaryanissac23 <aaryan.issac23@gmail.com>
2026-06-10 22:16:13 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;
+        int mid = 0;
+        while(l < r){
+            mid = l + (r-l)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] < target) l = mid+1;
+            else r = mid;
+        }
+        return l;
+    }
+}
